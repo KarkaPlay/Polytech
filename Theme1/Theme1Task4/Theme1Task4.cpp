2c85963c3f134ef49048eb3b37e0d0bce8e4e874
Sergey Karpushkin <c1e2r3g@gmail.com>
2020-09-20 21:44:30 +0300

    Решена задача 4

---
+++ Theme1/Theme1Task4/Theme1Task4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите два ненулевых числа: " << endl;
+    float a, b;
+    cin >> a >> b;
+    a = a * a;
+    b = b * b;
+    cout << "Сумма их квадратов - " << a + b << endl;
+    cout << "Разность их квадратов - " << a - b << endl;
+    cout << "Произведение их квадратов - " << a * b << endl;
+    cout << "Частное их квадратов - " << a / b;
+}

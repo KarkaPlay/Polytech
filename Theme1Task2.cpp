e40f93df4768c85e71e9f7f010bbdd8cbadcb097
KarpushkinS <71441902+KarpushkinS@users.noreply.github.com>
2020-09-20 21:31:44 +0300

    Add files via upload

---
+++ Theme1Task2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите диаметр окружности: " << endl;
+    int d;
+    cin >> d;
+    float L = 3.14 * d;
+    cout << "Длина окружности - " << L;
+}

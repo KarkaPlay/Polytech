0289b85a76aad6f3b97f6bbad7f625758d45dabe
Sergey Karpushkin <c1e2r3g@gmail.com>
2020-09-20 21:37:44 +0300

    Решена задача 3

---
+++ Theme1/Theme1Task3/Theme1Task3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите два числа: " << endl;
+    float a, b;
+    cin >> a >> b;
+    float avg = (a+b)/2;
+    cout << "Их среднее арифметическое - " << avg;
+}

c633480c593936b7df30ecb73d088865cbb06f50
Sergey Karpushkin <c1e2r3g@gmail.com>
2020-09-20 21:52:56 +0300

    Решена задача 5

---
+++ Theme1/Theme1Task5/Theme1Task5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите два ненулевых числа: " << endl;
+    float a, b;
+    cin >> a >> b;
+    a = abs(a);
+    b = abs(b);
+    cout << "Сумма их модулей - " << a + b << endl;
+    cout << "Разность их модулей - " << a - b << endl;
+    cout << "Произведение их модулей - " << a * b << endl;
+    cout << "Частное их модулей - " << a / b;
+}

775f41c25833282925cad2d445e68dab737417c6
KarpushkinS <c1e2r3g@gmail.com>
2020-09-25 01:09:20 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 6/Задание 3/Задание 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите три числа\n";
+	int A, B, C;
+	cout << "A = ";
+	cin >> A;
+	cout << "B = ";
+	cin >> B;
+	cout << "C = ";
+	cin >> C;
+
+	int t;
+	t = A;
+	A = B;
+	B = C;
+	C = t;
+
+	cout << "A = " << A << endl << "B = " << B << endl << "C = " << C;
+}

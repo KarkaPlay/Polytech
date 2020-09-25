a5d4e1ec43db5ebd185c927d72e1bef143090ae0
KarpushkinS <c1e2r3g@gmail.com>
2020-09-25 22:04:23 +0300

    Create Задание 7.cpp

---
+++ ВВП Тема 6/Задание 7/Задание 7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число А: ";
+    int a;
+    cin >> a;
+    int t = a * a; // t = a^2
+    int v = t * a; // v = a^3
+    t = v * t;     // t = a^5
+    v = t * t;     // v = a^10
+    a = v * t;     // a = a^15
+    cout << "A^15 = " << a;
+}

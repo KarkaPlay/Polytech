c60349c1d40351564fd7af616bbb53f3a2e13c06
KarpushkinS <c1e2r3g@gmail.com>
2020-09-25 20:48:51 +0300

    Create Задание 6.cpp

---
+++ ВВП Тема 6/Задание 6/Задание 6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число А: ";
+    int a;
+    cin >> a;
+    int t = a * a;
+    a = t * t;
+    a = a * a;
+    cout << "A^3 = " << a;
+}

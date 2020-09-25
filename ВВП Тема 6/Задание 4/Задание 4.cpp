279b2079ddeafc4da73d4d19a74461c1dd28c668
KarpushkinS <c1e2r3g@gmail.com>
2020-09-25 20:17:41 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 6/Задание 4/Задание 4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "y = 3x^6 - 6x^2 - 7" << endl << "Введите X: ";
+	int x;
+	cin >> x;
+	int y = pow(3*x, 6) - pow(6*x, 7) - 7;
+	cout << "y = " << y;
+}

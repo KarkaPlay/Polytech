2c591075e4cd8450df0beeaaca2dfa4f8dce58f5
KarpushkinS <c1e2r3g@gmail.com>
2020-09-25 20:32:24 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 6/Задание 5/Задание 5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "y = 4(x-3)^6 - 7(x-3)^3 + 2 " << endl << "Введите X: ";
+	int x;
+	cin >> x;
+	int y = 4*pow(x-3, 6) - 7*pow(x-3, 3) + 2;
+	cout << "y = " << y;
+}

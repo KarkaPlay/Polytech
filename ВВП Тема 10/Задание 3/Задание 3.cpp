eec135c07bca1f059ede88e2fb9af8fb66351bf1
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:36:42 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 10/Задание 3/Задание 3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число:\n";
+    int a;
+    cin >> a;
+    cout << "Данное число чётное двузначное: ";
+    if (a%2==0 and a>9 and a<100)
+        cout << "Высказывание истинно";
+    else
+        cout << "Высказывание ложно";
+}

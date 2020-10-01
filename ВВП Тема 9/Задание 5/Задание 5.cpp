f47269b16a91c40377777d54cc0ad706d2f5280c
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:09:47 +0300

    Задание 5

---
+++ ВВП Тема 9/Задание 5/Задание 5.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите номер года: ";
+    int year;
+    cin >> year;
+    cout << "Это " << (year-1) / 100 + 1 << "-е столетие";
+}

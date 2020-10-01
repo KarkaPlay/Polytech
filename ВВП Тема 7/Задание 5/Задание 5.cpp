b5022edbf45142c2685f2d10d7ff66e0cbbfe81f
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 18:40:06 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 7/Задание 5/Задание 5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "A*x + B = 0\n" << "Введите A: ";
+    int a;
+    cin >> a;
+   
+    cout << "Введите B: ";
+    int b;
+    cin >> b;
+
+    float x = (float)(-b / a);
+    cout << "X = " << x;
+}

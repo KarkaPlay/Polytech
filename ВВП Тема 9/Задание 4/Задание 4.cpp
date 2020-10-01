3f00847ae092027af72cad0122624fb0aa589078
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:02:28 +0300

    Задание 4

---
+++ ВВП Тема 9/Задание 4/Задание 4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите стороны прямоугольника:\n";
+    int a, b;
+    cin >> a >> b;
+
+    cout << "Введите сторону квадрата: ";
+    int c;
+    cin >> c;
+
+    int k = a / c * b / c;
+    cout << "В прямоугольник помещается " << k << " квадратов\n";
+    cout << "Незанятая площадь - " << a*b - k*c*c;
+}

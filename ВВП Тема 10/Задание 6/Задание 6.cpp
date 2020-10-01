52c57bfad842d0bd01f545334d55cb9ba1147ad2
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:56:33 +0300

    Create Задание 6.cpp

---
+++ ВВП Тема 10/Задание 6/Задание 6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите стороны треугольника\n";
+
+    cout << "a: ";
+    int a;
+    cin >> a;
+    a = a * a;
+
+    cout << "b: ";
+    int b;
+    cin >> b;
+    b = b * b;
+
+    cout << "c: ";
+    int c;
+    cin >> c;
+    c = c * c;
+
+    cout << "Данное число читается одинаково слева направо и справа налево: ";
+    if (a+b==c or b+c==a or a+c==b)
+        cout << "Высказывание истинно";
+    else
+        cout << "Высказывание ложно";
+}

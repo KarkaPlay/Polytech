da00e1e3c8b6e7e3b139b733cdf67b1160ca11ca
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 23:45:23 +0300

    Задание 3

---
+++ ВВП Тема 9/Задание 3/Задание 3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите K: ";
+    int k;
+    cin >> k;
+    cout << "Введите N: ";
+    int n;
+    cin >> n;
+    cout << "Номер дня недели - " << (k+n-2) % 7 + 1;
+}

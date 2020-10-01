237aa0b5443cba101bcd7c4ea3dbc6004ee95d02
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 22:01:52 +0300

    Задание 5

---
+++ ВВП Тема 8/Задание 5/Задание 5.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите трехзначное число: ";
+	int a;
+	cin >> a;
+
+	a = a/100 + a%100*10;
+	cout << a;
+}

1e3126665a5df6c86491d1406b58679fcd631ef0
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 17:49:36 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 7/Задание 3/Задание 3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "X кг конфет стоит A рублей\n" << "Введите X: ";
+    float x, a;
+    cin >> x;
+    cout << "Введите A: ";
+    cin >> a;
+    float for1kg = a/x;
+    cout << "1 кг стоит " << for1kg << " рублей\n";
+    cout << "Рассчёт стоимости Y кг этих же конфет\nВведите Y: ";
+    int y;
+    cin >> y;
+    float price = for1kg * y;
+    cout << y << " кг этих же конфет стоит " << price << " рублей";
+}

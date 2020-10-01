be814d87ebbfe7d884e84b8bb2300cad5dfdf306
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 19:27:38 +0300

    Сделано задание 1

---
+++ ВВП Тема 8/Задание 1/Задание 1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер файла в байтах: ";
+    int i;
+    cin >> i;
+    cout << "Размер файла в килобайтах - " << (float)i / 1024 << " Кб";
+}

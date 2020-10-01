fb1535886e9071e3117979ec1faac7496f8e6988
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 18:34:03 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 7/Задание 4/Задание 4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите скорость первой машины: ";
+    int v1;
+    cin >> v1;
+
+    cout << "Введите скорость второй машины: ";
+    int v2;
+    cin >> v2;
+
+    cout << "Введите расстояние между ними: ";
+    int s;
+    cin >> s;
+
+    cout << "Введите время: ";
+    int t;
+    cin >> t;
+
+    int s2 = s + t * (v1 + v2);
+    cout << "Между машинами будет " << s2 << " км";
+}

1698a4edb5ea56436b29630b946f721ace4cc131
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:48:45 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 10/Задание 5/Задание 5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите четырёхзначное число:\n";
+    int a;
+    cin >> a;
+    cout << "Данное число читается одинаково слева направо и справа налево: ";
+    if (a/1000 == a%10 and a/100%10 == a%100/10)
+        cout << "Высказывание истинно";
+    else
+        cout << "Высказывание ложно";
+}

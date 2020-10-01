66a7164463f32ce42aaa4321c0ef82d68ff697a1
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 23:18:28 +0300

    Задание 1

---
+++ ВВП Тема 9/Задание 1/Задание 1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите сколько секунд прошло с начала суток: ";
+    int sec;
+    cin >> sec;
+    cout << "С начала последней минуты прошло " << sec % 60 << " секунд";
+}

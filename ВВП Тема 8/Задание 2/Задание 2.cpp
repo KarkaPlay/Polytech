ed3c0aa0031d5d84db7f0283a4a57043d9a7f81a
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 21:46:33 +0300

    Задание 2

---
+++ ВВП Тема 8/Задание 2/Задание 2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите длину отрезка A: ";
+	int a;
+	cin >> a;
+
+	cout << "Введите длину отрезка B: ";
+	int b;
+	cin >> b;
+
+	cout << "В отрезок A помещается " << a / b << " отрезков B";
+}

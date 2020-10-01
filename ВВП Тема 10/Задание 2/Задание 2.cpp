1c1193268fbc22ecc020be23b12a4e2bdbadfb3b
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:33:08 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 10/Задание 2/Задание 2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите A, B и C:\n";
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "A < B < C: ";
+    if (a < b and b < c)
+        cout << "Высказывание истинно";
+    else
+        cout << "Высказывание ложно";
+}

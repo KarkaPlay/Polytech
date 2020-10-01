167a497052aa02d254b04d4938a6d6008d5b434c
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 23:36:32 +0300

    Задание 2

---
+++ ВВП Тема 9/Задание 2/Задание 2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите K: ";
+    int k;
+    cin >> k;
+    cout << "Номер дня недели - " << k % 7;
+}

bc30b3522464928837f7ac97cb46d0e0745c1214
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 00:45:11 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 10/Задание 4/Задание 4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите трёхзначное число:\n";
+    int a;
+    cin >> a;
+    cout << "Цифры данного числа образуют возрастающую или убывающую последовательность: ";
+    if ((a/100 < a/10%10 and a/10%10 < a%10) or (a/100 > a/10%10 and a/10%10 > a%10))
+        cout << "Высказывание истинно";
+    else
+        cout << "Высказывание ложно";
+}

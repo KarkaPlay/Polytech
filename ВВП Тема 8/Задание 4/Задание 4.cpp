0b3c4da7ff44b09dce1fb0d339a9314e27960f76
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 21:57:42 +0300

    Задание 4

---
+++ ВВП Тема 8/Задание 4/Задание 4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите двухзначное число: ";
+	int a;
+	cin >> a;
+
+	a = (a % 10 * 10) + a / 10;
+	cout << a;
+}

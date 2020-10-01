bd4f8de097a4cf6e54cf190fa5b0557e53da6d1b
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 17:15:07 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 7/Задание 2/Задание 2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите угол в радианах (без Pi): ";
+    float rad;
+    cin >> rad;
+    float a = rad * 180;
+    cout << rad << " * Pi = " << a << "°";
+}

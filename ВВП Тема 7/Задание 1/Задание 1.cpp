0fd23b2e42f403cae004d38ad6abd94e9f65b492
KarpushkinS <c1e2r3g@gmail.com>
2020-10-01 17:09:55 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 7/Задание 1/Задание 1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите угол в градусах: ";
+    int a;
+    cin >> a;
+    float rad = (float)a / 180;
+    cout << a << "° = " << rad << " * Pi радиан ";
+}

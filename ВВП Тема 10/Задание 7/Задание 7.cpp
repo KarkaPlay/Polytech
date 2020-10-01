0e7a450fee41664a0eeb7424cad709f995a4188d
KarpushkinS <c1e2r3g@gmail.com>
2020-10-02 01:04:32 +0300

    Create Задание 7.cpp

---
+++ ВВП Тема 10/Задание 7/Задание 7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите стороны треугольника\n";
+
+    cout << "a: ";
+    int a;
+    cin >> a;
+
+    cout << "b: ";
+    int b;
+    cin >> b;
+
+    cout << "c: ";
+    int c;
+    cin >> c;
+
+    cout << "Существует треугольник со сторонами a, b, c: ";
+    if (a + b > c and b + c > a and a + c > b)
+        cout << "Высказывание истинно";
+    else
+        cout << "Высказывание ложно";
+}

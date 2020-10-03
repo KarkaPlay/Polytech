5e4c483b821f1c2cb22b4b587becee95126f6c38
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 16:30:52 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 11/Задание 5/Задание 5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число: ";
+    int x;
+    cin >> x;
+    string d = "";
+    if (x == 0)
+        d += "Нулевое ";
+    else
+    {
+        if (x > 0)
+            d += "Положительное ";
+        else
+            d += "Отрицательное ";
+
+        if (x % 2 == 0)
+            d += "чётное ";
+        else
+            d += "нечётное ";
+    }
+
+    d += "число";
+
+    cout << "x - " << d;
+}

f323fe8cdf16946cee538effdc3e297705f0439a
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 15:23:16 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 11/Задание 2/Задание 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите три числа: \n";
+    int a, b, c, sum;
+    cin >> a >> b >> c;
+    
+    if (a < b)
+    {
+        if (a < c) // a - наименьшее
+            sum = b + c;
+        else       // c - наименьшее
+            sum = b + a;
+    }
+    else // b < a
+    {
+        if (b < c) //  b - наименьшее
+            sum = a + c;
+        else       // c - наименьшее
+            sum = a + b;
+    }
+    cout << "Сумма двух наибольших - " << sum;
+}

ce150f48de5f70ebef31e119f37821bac20deb65
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 23:31:11 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 13/Задание 5/Задание 5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите целое число N: ";
+    cin >> n;
+    float a;
+    cout << "Введите вещественное число A: ";
+    cin >> a;
+
+    float sum = 0;
+    for (int i = 0; i <= n; i=i+2)
+    {
+        sum += pow(a, i);
+        sum -= pow(a, i + 1);
+    }
+    cout << "Сумма - " << sum;
+}

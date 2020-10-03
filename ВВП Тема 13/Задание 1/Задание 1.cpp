749a2f5797ff5e1f36a1e5307f4626b89ad9ed8d
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 23:05:36 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 13/Задание 1/Задание 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float s;
+    cout << "Введите цену 1 кг конфет: ";
+    cin >> s;
+    for (float i = 0.1; i < 1.1; i = i + 0.1) 
+    {
+        cout << i << " кг - " << s * i * 10 << endl;
+    }
+}

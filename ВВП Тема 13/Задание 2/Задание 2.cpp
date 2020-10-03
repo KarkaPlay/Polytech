8b3ff8833b768309992727119eaccc090c6e651d
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 23:11:40 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 13/Задание 2/Задание 2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите целое число N: ";
+    cin >> n;
+    float a = 1;
+    for (int i = 0; i < n; i++)
+    {
+        float k = 1.1;
+        a *= k;
+        k = k + .1;
+    }
+    cout << a;
+}

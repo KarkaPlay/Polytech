e23f32635195c1df88b3c16c419754e37ab5b30f
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 16:19:39 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 11/Задание 4/Задание 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите координаты точки: \n";
+    int x, y;
+
+    cout << "x: ";
+    cin >> x;
+
+    cout << "y: ";
+    cin >> y;
+
+    if      (x > 0 and y > 0)
+        cout << "1-я четверть";
+    else if (x < 0 and y > 0)
+        cout << "2-я четверть";
+    else if (x < 0 and y < 0)
+        cout << "3-я четверть";
+    else
+        cout << "4-я четверть";
+}

d3c45de2e486dae9e3b5d1e8f3831c059fa000f5
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 15:10:24 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 11/Задание 1/Задание 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите переменные A и B: \n";
+    int a, b;
+    cin >> a >> b;
+    if (a != b)
+    {
+        if (a > b)
+            b = a;
+        else
+            a = b;
+    }
+    else
+    {
+        a = b = 0;
+    }
+    cout << "A = " << a << "\nB = " << b;
+}

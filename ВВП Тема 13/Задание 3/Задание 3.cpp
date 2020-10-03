fef836b2e86b0b38f2405c815e7dfe59edcddd88
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 23:17:20 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 13/Задание 3/Задание 3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите целое число N: ";
+    cin >> n;
+    int s = 0;
+    for (int i = 1; i <= 2*n-1; i=i+2)
+    {
+        s += i;
+        cout << endl << s;
+    }
+    cout << "\nN^2 = " << s;
+}

baee47a8b3ce4ba9f6ef3eda1c78f3267d227ef2
KarpushkinS <c1e2r3g@gmail.com>
2020-10-03 23:28:33 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 13/Задание 4/Задание 4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите целое число N: ";
+    cin >> n;
+    float a;
+    cout << "Введите вещественное число A: ";
+    cin >> a;
+
+    float sum = 0;
+    for (int i = 0; i <= n; i++)
+        sum += pow(a, i);
+    cout << "Сумма - " << sum;
+}

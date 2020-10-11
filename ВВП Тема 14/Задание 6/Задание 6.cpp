3f0fc86b5034485961c84597eea6a3cfc2ee9f8f
KarpushkinS <c1e2r3g@gmail.com>
2020-10-11 23:59:39 +0300

    Create Задание 6.cpp

---
+++ ВВП Тема 14/Задание 6/Задание 6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число Фибоначчи:\n";
+    int n, k=3, f0=1, f1=1, f2=2;
+    cin >> n;
+
+    while (f2 != n)
+    {
+        f0 = f1;
+        f1 = f2;
+        f2 = f0 + f1;
+        k++;
+    }
+    cout << "Его порядковый номер - " << k;
+}

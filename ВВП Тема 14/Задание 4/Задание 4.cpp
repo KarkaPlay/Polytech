b2655aae18f2bb0d0fb41eeb758c9bb83e0e1369
KarpushkinS <c1e2r3g@gmail.com>
2020-10-11 23:33:13 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 14/Задание 4/Задание 4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите P: ";
+    float p, s = 1000;
+    int k=0;
+    cin >> p;
+    p /= 100; // переводим проценты в доли
+
+    while (s <= 1100)
+    {
+        s += s * p;
+        k++;
+    }
+    cout << "Количество месяцев - " << k;
+    cout << "\nИтоговый размер вклада - " << s;
+}

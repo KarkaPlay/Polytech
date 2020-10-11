dceeb850b167852f1482ad2fc18f52998513ec6a
KarpushkinS <c1e2r3g@gmail.com>
2020-10-11 23:07:50 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 14/Задание 3/Задание 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите целое число N>1: ";
+    int n;
+    cin >> n;
+
+    int s = 0, k=0;
+    while (s < n)
+    {
+        k++;
+        s += k;
+    }
+    cout << k;
+}

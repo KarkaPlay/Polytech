9e4c2afb5a4a3419b9b303718ec89e3e56ab1b0e
KarpushkinS <c1e2r3g@gmail.com>
2020-10-11 21:33:00 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 14/Задание 1/Задание 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите два целых положительных числа:\n";
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++)
+        for (int j = 0; j < i; j++)
+            cout << i << endl;
+}

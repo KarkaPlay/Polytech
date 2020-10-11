3cd676ec06c844899f98da3d4f2a34dff38f9237
KarpushkinS <c1e2r3g@gmail.com>
2020-10-11 21:47:39 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 14/Задание 2/Задание 2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите два положительных числа:\n";
+    float a, b;
+    cin >> a >> b;
+
+    while (a >= b) 
+        a -= b;
+    cout << "Незанято " << a;
+}

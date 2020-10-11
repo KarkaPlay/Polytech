8a95e2c69a749215f47140cb5f70e39fa2c3f140
KarpushkinS <c1e2r3g@gmail.com>
2020-10-11 23:59:36 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 14/Задание 5/Задание 5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите два целых положительных числа:\n";
+    int a, b;
+    cin >> a >> b;
+
+    while (a != 0 and b != 0)
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+    cout << "НОД: " << a + b;
+}

84694e7f08c25ad31303b9ac775c85e4c0462ce6
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 01:53:34 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 15/Задание 1/Задание 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+float PowerA3(float a, float b)
+{
+    b = a * a * a;
+    return b;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число:\n";
+    int a;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a;
+        cout << a << "^3 = " << PowerA3(a, 0) << endl;
+    }
+}

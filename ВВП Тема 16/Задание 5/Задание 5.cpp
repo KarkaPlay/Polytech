4ca453eddd9037f3ce588c68190ef8c47f0a73f5
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 14:49:43 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 16/Задание 5/Задание 5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* A = new int[n];
+
+    cout << "Введите элементы массива: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i = 1; i < n; i+=2)
+        cout << A[i] << " ";
+    cout << "| ";
+    for (int i = ((n-1)/2)*2; i >= 0; i -= 2)
+        cout << A[i] << " ";
+}

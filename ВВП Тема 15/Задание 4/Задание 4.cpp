47b0a820222d92a6abedd9f84cc5860f2a75d990
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 11:47:27 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 15/Задание 4/Задание 4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+float x, y;
+
+int Quarter(float x, float y)
+{
+    if (x > 0 and y > 0)
+        return 1;
+    else if (x < 0 and y>0)
+        return 2;
+    else if (x < 0 and y < 0)
+        return 3;
+    else if (x > 0 and y < 0)
+        return 4;
+    else
+        cout << "Неверные координаты";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Введите координаты\nx:";
+        cin >> x;
+        cout << "y:";
+        cin >> y;
+        cout << "Это " << Quarter(x, y) << "-я четверть\n";
+    }
+}

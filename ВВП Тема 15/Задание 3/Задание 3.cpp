9ac336293ad8b978db963447b4e63fd019cd9dc9
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 11:01:17 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 15/Задание 3/Задание 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double PI = acos(-1.0);
+float r1, r2;
+
+float RingS(float r1, float r2)
+{
+    return (PI*pow(r1,2) - PI * pow(r2, 2));
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите R1: ";
+    cin >> r1;
+    cout << "Введите R2: ";
+    cin >> r2;
+    cout << "Радиус кольца = " << RingS(r1, r2);
+}

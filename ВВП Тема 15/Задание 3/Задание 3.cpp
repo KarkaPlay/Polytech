e8fb5931b64fdbacad6bf52b56559a4a30a4d5a4
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 11:12:37 +0300

    Update Задание 3.cpp

--- ВВП Тема 15/Задание 3/Задание 3.cpp
+++ ВВП Тема 15/Задание 3/Задание 3.cpp
@@ -6,15 +6,18 @@ float r1, r2;
 
 float RingS(float r1, float r2)
 {
-    return (PI*pow(r1,2) - PI * pow(r2, 2));
+    cout << "Введите R1: ";
+    cin >> r1;
+    cout << "Введите R2: ";
+    cin >> r2;
+    return abs(PI*r1*r1 - PI * r2*r2);
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    cout << "Введите R1: ";
-    cin >> r1;
-    cout << "Введите R2: ";
-    cin >> r2;
-    cout << "Площадь кольца = " << RingS(r1, r2);
+    
+    cout << "Площадь первого кольца = " << RingS(r1, r2) << endl;
+    cout << "Площадь второго кольца = " << RingS(r1, r2) << endl;
+    cout << "Площадь третьего кольца = " << RingS(r1, r2);
 }

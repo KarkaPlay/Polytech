75315a23310419a3d777acce8308ec8e58070915
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 14:38:10 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 16/Задание 4/Задание 4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива:\n";
+    int n;
+    cin >> n;
+    int* A = new int[n];
+
+    cout << "Введите элементы массива: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i = n-1; i > n/2; i--)
+    {
+        cout << A[abs(i-n+1)] << " ";
+        cout << A[i] << " ";
+    }
+    if (n % 2 != 0)
+        cout << A[n/2] << " ";
+    else
+        cout << A[(n/2)-1] << " " << A[n/2];
+}

0022d5a956b44b6aaae6f447387b7e9b1dceaa6d
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 13:50:40 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 16/Задание 1/Задание 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите целое положительное число:\n";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        N[i] = 2 * i + 1;
+        cout << N[i]  << " ";
+    }
+}

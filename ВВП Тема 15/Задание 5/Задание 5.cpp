412f24b3b20914aa5a6a05198c4265b8522394b2
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 12:13:12 +0300

    Create Задание 5.cpp

---
+++ ВВП Тема 15/Задание 5/Задание 5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+float Fact2(int n)
+{
+    int i;
+    float fac=1;
+    if (n % 2 == 0)
+        i = 2;
+    else
+        i = 1;
+
+    while (i <= n)
+    {
+        fac *= i;
+        i += 2;
+    }
+    return fac;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число:\n";
+    cin >> n;
+    cout << "N!! = " << Fact2(n);
+}

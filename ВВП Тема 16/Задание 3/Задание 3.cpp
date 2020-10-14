cc1382091de2c50dbe3d8ac27815ce461b041232
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 14:17:14 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 16/Задание 3/Задание 3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    cout << "Введите первое и второе числа массива:\n";
+    int a, b;
+    cin >> a >> b;
+
+    N[0] = a;
+    N[1] = b;
+    int sum = a + b;
+    cout << N[0] << " " << N[1] << " ";
+
+    for (int i = 2; i < n; i++)
+    {
+        N[i] = sum;
+        sum += N[i];
+        cout << N[i] << " ";
+    }
+}

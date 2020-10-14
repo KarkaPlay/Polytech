9ec8dd7ed1f4b344b3475801fcc66561258a30fc
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 14:00:01 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 16/Задание 2/Задание 2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+
+    cout << "Введите первый член прогрессии: ";
+    int a;
+    cin >> a;
+
+    cout << "Введите знаменатель прогрессии: ";
+    int d;
+    cin >> d;
+
+    int* N = new int[n];
+    N[0] = a;
+    cout << N[0] << " ";
+
+    for (int i = 1; i < n; i++)
+    {
+
+        N[i] = a * pow(d,i);
+        cout << N[i] << " ";
+    }
+}

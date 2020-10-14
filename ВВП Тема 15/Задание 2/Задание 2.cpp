e10216b2ef24b81856c22163c40bffa1777626b0
KarpushkinS <c1e2r3g@gmail.com>
2020-10-14 10:33:57 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 15/Задание 2/Задание 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sign(float x)
+{
+    if (x < 0)
+        return -1;
+    else if (x == 0)
+        return 0;
+    else
+        return 1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите две переменные:\n";
+    float a, b;
+    cin >> a >> b;
+    cout << Sign(a) + Sign(b);
+}
+

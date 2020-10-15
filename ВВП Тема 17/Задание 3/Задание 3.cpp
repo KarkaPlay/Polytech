f79637a9936c200afbd4e3f29d9d4bf89d59e57f
KarpushkinS <c1e2r3g@gmail.com>
2020-10-15 13:55:46 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 17/Задание 3/Задание 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    cout << "Введите элементы массива:\n";
+    for (int i = 0; i < n; i++)
+        cin >> N[i];
+
+    int min = 1000;
+    for (int i = 0; i < n; i += 2)
+        if (N[i] < min)
+            min = N[i];
+    cout << "Минимальный элемент - " << min;
+}

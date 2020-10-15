5c17f4cba516a6dbfdd7810f823901117dfe87ce
KarpushkinS <c1e2r3g@gmail.com>
2020-10-15 13:23:49 +0300

    Create Задание 1.cpp

---
+++ ВВП Тема 17/Задание 1/Задание 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    cout << "Введите элементы массива:\n";
+    for (int i = 0; i < n; i++)
+        cin >> N[i];
+
+    cout << "Введите два числа:\n";
+    int k, l;
+    cin >> k >> l;
+
+    float avg = 0;
+    for (int i = k; i <= l; i++)
+    {
+        avg += N[i];
+    }
+    avg /= (l - k + 1);
+
+    cout << "Среднее арифметическое чисел с номерами от " << k << " до " << l << " - " << avg;
+}

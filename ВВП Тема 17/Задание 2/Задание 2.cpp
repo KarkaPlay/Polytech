0585cabf3017d0ea2cbc946c43970b7efef636aa
KarpushkinS <c1e2r3g@gmail.com>
2020-10-15 13:38:25 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 17/Задание 2/Задание 2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    cout << "Введите элементы массива:\n";
+    for (int i = 0; i < n; i++)
+        cin >> N[i];
+
+    bool a = true;
+    for (int i = 2; i < n; i++)
+    {
+        if (N[i] - N[i-1] != N[i-1] - N[i-2])
+            a = false;
+    }
+
+    if (a == true)
+        cout << "Массив содержит арифметическую прогрессию";
+    else
+        cout << "Массив не содержит арифметическую прогрессию";
+}

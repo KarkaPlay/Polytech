2c42431cdc54a59d5b24b031c960826e6dd4bdc4
KarpushkinS <c1e2r3g@gmail.com>
2020-10-15 16:25:52 +0300

    Create Задание 2.cpp

---
+++ ВВП Тема 18/Задание 2/Задание 2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int& a, int& b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* A = new int[n];
+
+    cout << "Введите элементы массива A: ";
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    float* B = new float[n];
+    int sum = 0;
+    cout << "Массив B: ";
+    for (int i = 0; i < n; i++)
+    {
+        sum += A[i];
+        B[i] = (float)sum / (i + 1);
+        cout << B[i] << " ";
+    }
+}

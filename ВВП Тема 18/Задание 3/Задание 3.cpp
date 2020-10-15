01859eb6644c12cae72932753c75382080bf787d
KarpushkinS <c1e2r3g@gmail.com>
2020-10-15 19:08:59 +0300

    Create Задание 3.cpp

---
+++ ВВП Тема 18/Задание 3/Задание 3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int& a, int& b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < n; i++)
+        cin >> N[i];
+
+    int odd=2;
+    for (int i = 0; i < n; i++)
+        if (N[i] % 2 != 0)
+            odd = N[i];
+    cout << "Новый массив: ";
+    for (int i = 0; i < n; i++)
+    {
+        if (odd != 2)
+            if (N[i]%2 != 0)
+                N[i] += odd;
+        cout << N[i] << " ";
+    }
+}

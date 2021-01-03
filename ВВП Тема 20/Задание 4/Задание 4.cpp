5a5a5788787f3a23c01efa2f7b8fbfeb7ef86795
KarpushkinS <c1e2r3g@gmail.com>
2021-01-03 18:18:26 +0300

    Create Задание 4.cpp

---
+++ ВВП Тема 20/Задание 4/Задание 4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    struct dot
+    {
+        int x;
+        int y;
+        float distance;
+    };
+
+    setlocale(LC_ALL, "Russian");
+    int N;
+    cout << "Введите количество точек: ";
+    cin >> N;
+    dot* A = new dot[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Точка " << i + 1 << endl;
+        cout << "x: ";
+        cin >> A[i].x;
+        cout << "y: ";
+        cin >> A[i].y;
+    }
+
+    dot maxDistanceDot{ 0, 0, 0 };
+    int maxi = -1;
+    for (int i = 0; i < N; i++)
+        if ((A[i].x < 0) and (A[i].y > 0))
+        {
+            A[i].distance = sqrt((A[i].x * A[i].x) + (A[i].y * A[i].y));
+            if (A[i].distance > maxDistanceDot.distance)
+            {
+                maxDistanceDot = A[i];
+                maxi = i + 1;
+            }
+        }
+    if (maxi == -1)
+        cout << "Во второй четверти точек нет.";
+    else
+        cout << "Наиболее удалена Точка " << maxi << ", её координаты:";
+    cout << endl << "x: " << maxDistanceDot.x << "  y: " << maxDistanceDot.y;
+    cout << endl << "Дистанция: " << maxDistanceDot.distance;
+}

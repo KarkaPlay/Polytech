fd4a135a96cebcb4f3731127fbc8694da2411441
KarpushkinS <c1e2r3g@gmail.com>
2021-02-16 16:35:36 +0300

    Create ВВП Тема 21.cpp

---
+++ Задание 1/ВВП Тема 21/ВВП Тема 21.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите порядок матрицы: ";
+    int M;
+    cin >> M;
+    int** A = new int*[M];
+    for (int i = 0; i < M; i++)
+        A[i] = new int[i + 1];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << A[i][j];
+        }
+        cout << endl;
+    }
+}
